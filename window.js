a073e63ab6bc55aa340df4f68926db77b3842325
YongHui Cho <ycho46@gmail.com>
2018-10-29 00:24:58 -0400

    binded each value to update each currency field depending on input

--- window.js
+++ window.js
@@ -24,16 +24,25 @@ $(() => {
                 map[result['gesmes:Envelope'].Cube[0].Cube[0].Cube[curr].$.currency] = result['gesmes:Envelope'].Cube[0].Cube[0].Cube[curr].$.rate;
             }
 //            prints all contents of map
-            for(k in map) {
-                app.console.log(k + " " + map[k]);
-            }
+//            for(k in map) {
+//                app.console.log(k + " " + map[k]);
+//            }
         });
     })
     .catch(function(err) {
       //handle error
     })
+    
+    $('#number-input').bind('input propertychange', function() {
+        const num = this.value;
+        const usd = Number(map['USD'] * num).toFixed(2);
+        $('#usd').text(usd);
+        const jpy = Number(map['JPY'] * num).toFixed(2);
+        $('#jpy').text(jpy);   
+        const gbp = Number(map['GBP'] * num).toFixed(2);
+        $('#gbp').text(gbp);
+        const cny = Number(map['CNY'] * num).toFixed(2);
+        $('#cny').text(cny);
+    })
+    $('#text-input').focus() // focus input box
 });
-
-printOption(option) => {
-    app.console.log(option);
-}
